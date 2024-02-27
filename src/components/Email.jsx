30e474ad68882f24eb7cb68589bc5f8b989f7948
Dimple Varshney <dimplevarshney06@gmail.com>
2024-02-28 00:01:16 +0530

    login-signup pages

--- src/components/Email.jsx
+++ src/components/Email.jsx
@@ -4,7 +4,7 @@ const Email = ({ email, emailSubDivision, style }) => {
   console.log("email, emailSubDivision_", email, emailSubDivision);
   const formattedDate = formatDate(email.timestamp);
     return (
-      <div style={{ padding: "0px", display:"flex", justifyContent: "space-around",boxShadow: "0 -0.5px 0 0 #333, 0 0.5px 0 0 #333", background: "#fff" }}>
+      <div className="flex p-3 justify-around bg-white" style={{boxShadow: "0 -0.5px 0 0 #333, 0 0.5px 0 0 #333"}}>
         <p>{emailSubDivision.name}</p>
         <p>{email.subject}</p>
         <p>{email.message}</p>
